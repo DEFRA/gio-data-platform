be153743e6c16a8342608e863e7c5c9bb45579a7
Kriss Sulikowski <kriss.sulikowski@capgemini.com>
2024-04-30 14:48:46 +0100

    Update Extract pipeline

--- src/infrastructure/scripts/PowerShell/Scripts/Purview/ExtractPurviewCustomisations.ps1
+++ src/infrastructure/scripts/PowerShell/Scripts/Purview/ExtractPurviewCustomisations.ps1
@@ -29,6 +29,9 @@ param (
     [Parameter(Mandatory = $true)]
     [string]$AdoAccountUrl
 
+    [Parameter(Mandatory = $true)]  
+    [string]$AdoRepositoryName
+
 )
 
 Import-Module $PSScriptRoot/../../Modules/Purview/PurviewModule.psm1
@@ -103,4 +106,4 @@ git -c http.extraheader="AUTHORIZATION: bearer $($AdoAccessToken)" push origin -
 #ADO Create a PR Automatically
 $env:AZURE_DEVOPS_EXT_PAT = $AdoAccessToken
 
-az repos pr create --auto-complete false --bypass-policy false --delete-source-branch true --description 'Extracted latest Changes from Purview' --source-branch $branch --squash true --target-branch main --title "PR for Purview Config $AccountName" --project $AdoProject --org $AdoAccountUrl --repository "GIO_DATA_PLATFORM"
+az repos pr create --auto-complete false --bypass-policy false --delete-source-branch true --description "Extracted latest Changes from Purview $AccountName" --source-branch $branch --squash true --target-branch main --title "PR for Purview Config $AccountName" --project $AdoProject --org $AdoAccountUrl --repository $AdoRepositoryName
